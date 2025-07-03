34fbfc9ffb15a674aeffb18c8086767b3362b85d
Aditi <aditisbadkur@gmail.com>
2025-07-03 16:35:51 +0530

    static form made

---
+++ force-app/main/default/lwc/leadGeneration/leadGeneration.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class LeadGeneration extends LightningElement {
+    // get phone number and usse recordId? maybe usse populate the name and age  
+    handleVerification(){
+        console.log("this is working");
+    }
+}
