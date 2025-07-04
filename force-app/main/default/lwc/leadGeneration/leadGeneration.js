70d25d2abf01381afec93c843fd80b4d48e757d3
Aditi <aditisbadkur@gmail.com>
2025-07-04 11:33:00 +0530

    aadhar, pan, CIBIL

--- force-app/main/default/lwc/leadGeneration/leadGeneration.js
+++ force-app/main/default/lwc/leadGeneration/leadGeneration.js
@@ -6,29 +6,55 @@ export default class LeadGeneration extends LightningElement {
     applicantName = '';
     applicantAge = '';
 
+    @track verified = false; // check this
+    @track panCard = '';
+    @track applicantAadhar = '';
+
+    // @track formDisabled = true;
+
+    applicantCIBIL = '';
+
     handleChange(event) {
         const field = event.target.name;
-        if (field === 'phoneNumber') {
-            this.phoneNumber = event.target.value;
-        }
-    }       
+        this[field] = event.target.type === 'number' 
+        ? event.target.value.toString() 
+        : event.target.value;
+    }
 
     connectedCallback() {
         console.log("Component connected");
         this.handleVerification();
     }
-
     // ⚠️ hard-coded hai 
     // irl fetch data from api using named credentials?
 
     handleVerification(){
-        console.log("this is working");
+        console.log("verification is working");
         if(this.phoneNumber == '9867187591'){
+            this.verified = true;
+            // formDisabled = false;
             console.log("checked phone number");
             this.applicantName = 'Nisha';
             this.applicantAge = '23';
             console.log("Name: " + this.applicantName);
             console.log("Age: " + this.applicantAge);
         }
+        if(this.verified){
+            this.handleDocuments();
+        }
+    }
+
+    handleDocuments(){
+        console.log("documents is working");
+        if(this.applicantAadhar == '123456789012'){
+            this.applicantCIBIL = '750';
+            console.log("Aadhar working");
+            console.log("CIBIL Score: " + this.applicantCIBIL);
+        } 
+        if(this.panCard == 'ABCDE1234F'){
+            this.applicantCIBIL = '650';
+            console.log("PAN Card working");
+            console.log("CIBIL Score: " + this.applicantCIBIL);
+        }  
     }
 }
