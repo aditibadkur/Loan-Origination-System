fcb67a4f7aa5e7d1e0829de48a4a97f4a189c02c
Aditi <aditisbadkur@gmail.com>
2025-07-09 18:16:24 +0530

    everything except dynamic working in emi calc

--- force-app/main/default/lwc/calculatorEMI/calculatorEMI.js
+++ force-app/main/default/lwc/calculatorEMI/calculatorEMI.js
@@ -2,10 +2,16 @@ import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class CalculatorEMI extends LightningElement {
+
+    readOnly = true;    
+
     @track loanAmount = 0;
     @track interestRate = 0;
     @track tenure = 0;
     @track emi = 0;
+
+    @track tenureSlider = 5;
+    @track rateSlider = 7;
 
     handleChange(event) {
         const field = event.target.name;
@@ -14,24 +20,28 @@ export default class CalculatorEMI extends LightningElement {
         : event.target.value;
     }
 
+    connectedCallback(){
+        this.calculateEMI();
+    }
+
     // E = [P x R x (1+R) ^N] / [(1+R) ^ (N-1)]
     calculateEMI() { 
         console.log("calculateEMI is working");
-        if(!this.loanAmount || !this.interestRate || !this.tenure) {
+        if(!this.loanAmount || !this.rateSlider || !this.tenureSlider) {
             this.showToast('Error', 'Please fill in all fields.', 'error');
             return;
         }
-        if(parseInt(this.loanAmount) <= 0 || parseInt(this.interestRate) <= 0 || parseInt(this.tenure) <= 0) {
+        if(parseInt(this.loanAmount) <= 0 || parseInt(this.rateSlider) <= 0 || parseInt(this.tenureSlider) <= 0) {
             this.showToast('Error', 'Loan Amount, Interest Rate, and Tenure must be greater than zero.', 'error');
             return;
         }   
-        this.interestRate = parseInt(this.interestRate) / 100;
-        this.tenure = parseInt(this.tenure) * 12; 
+        this.rateSliderValue = parseInt(this.rateSlider) / 100;  
+        this.tenureSliderValue = parseInt(this.tenureSlider) * 12; 
         this.loanAmount = parseInt(this.loanAmount);
-        this.emi = (this.loanAmount * this.interestRate * Math.pow((1 + this.interestRate), this.tenure)) / (Math.pow((1 + this.interestRate), this.tenure) - 1);
+        this.emi = (this.loanAmount * this.rateSliderValue * Math.pow((1 + this.rateSliderValue), this.tenureSliderValue)) / (Math.pow((1 + this.rateSliderValue), this.tenureSliderValue) - 1);
         this.emi = Math.ceil(this.emi);
         console.log("EMI: " + this.emi);
-        this.showToast('Success', `Your EMI is ${this.emi}`, 'success');
+        // this.showToast('Success', `Your EMI is ${this.emi}`, 'success');
     }
 
     showToast(title, message, variant) {
