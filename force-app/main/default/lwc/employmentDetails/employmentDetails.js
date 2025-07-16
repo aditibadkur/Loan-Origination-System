5da3c3b5926a4fc4838d2e8dcd7643847bc41a1d
Aditi <aditisbadkur@gmail.com>
2025-07-16 16:44:08 +0530

    below submit button aa rha file uploaded (only 1)

--- force-app/main/default/lwc/employmentDetails/employmentDetails.js
+++ force-app/main/default/lwc/employmentDetails/employmentDetails.js
@@ -14,6 +14,8 @@ import BUSINESS_FIELD from '@salesforce/schema/Applicant_Employment_Data__c.Busi
 import RELATION_FIELD from '@salesforce/schema/Applicant_Employment_Data__c.Relation_to_C__c';
 import QUALIFICATION_FIELD from '@salesforce/schema/Applicant_Employment_Data__c.Qualification__c';
 
+import fetchFiles from '@salesforce/apex/fileUpload.fetchFiles';
+
 export default class EmploymentDetails extends LightningElement {
     @track formVisible = false; // for next page!
     readOnly = true;
@@ -36,6 +38,7 @@ export default class EmploymentDetails extends LightningElement {
     @track relationOptions = [];
 
     @track acceptedFormats = ['.pdf'];
+    @track showFileComponent = false;
 
     handleUploadFinished(event) {
         const uploadedFiles = event.detail.files;
@@ -49,6 +52,76 @@ export default class EmploymentDetails extends LightningElement {
         if (fileComponent) {
             fileComponent.getFiles();
         }
+    }
+
+    get fileVisible(){
+        return this.showFileComponent;
+    }
+
+    @track isFileLoaded = true;
+    @track files;
+    @track error;
+    _lastRecordIdFetched;
+
+    @track previewUrl = '';
+    @track showPreview = false;
+
+    renderedCallback() {
+        if (this.showFileComponent) {
+            if (this.applicantid && this.applicantid !== this._lastRecordIdFetched) {
+                this.getFiles();
+                this._lastRecordIdFetched = this.applicantid;
+            }
+        }
+    }
+    
+    @api // on every upload call hota from parent, hence api is needed (so that all files are fetched)
+    getFiles() { 
+        fetchFiles({ recordId: this.applicantid })
+            .then(result => {
+                // isse size aane lag gaya!!!!! (since method directly call nhi hota, toh add as property to each file)
+                this.files = result.map(file => ({
+                    ...file, // get all deets of file
+                    formattedSize: this.formattedSize(file.ContentDocument.ContentSize)
+                }));
+                this.error = undefined;
+                if (!result || result.length === 0) {
+                    this.isFileLoaded = false;
+                    this.showToast('No Files', 'No files found for this record.', 'info');
+                } else {
+                    this.isFileLoaded = true;
+                }
+            })
+            .catch(error => {
+                this.error = error;
+                this.files = undefined;
+                this.isFileLoaded = false;
+                this.showToast('Error', 'Error fetching files: ' + (error.body?.message || error.message), 'error');
+            });
+    }
+    
+    handleViewFile(event) {
+        const contentDocumentId = event.currentTarget.dataset.id;
+        this.previewUrl = `/sfc/servlet.shepherd/document/preview/${contentDocumentId}`;
+        this.showPreview = true;
+        // Salesforce standard file viewer URL
+        // window.open(`/sfc/servlet.shepherd/document/preview/${contentDocumentId}`, '_self'); // not working same page preview
+        // window.open(`/sfc/servlet.shepherd/document/download/${contentDocumentId}`, '_blank'); // opens in new tab
+    }
+
+    formattedSize(size) {
+        if (size < 1024) return size + ' B';
+        if (size < 1048576) return Math.round(size / 102.4) / 10 + ' KB';
+        return Math.round(size / 104857.6) / 10 + ' MB';
+    }
+
+    showToast(title, message, variant) {
+        this.dispatchEvent(new ShowToastEvent({ title,  message, variant }));
+    }
+
+    handleClosePreview() {
+        this.showPreview = false;
+        this.previewUrl = '';
     }
 
     get employmentOptions(){
